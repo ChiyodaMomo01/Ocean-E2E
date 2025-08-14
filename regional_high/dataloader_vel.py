04b2248a1ce0e8130b68fd6d8cb0e6abbc41fdb4
Ruiqi Shu <126428563+ChiyodaMomo01@users.noreply.github.com>
2025-08-14 11:32:32 +0800

    Update dataloader_vel.py

--- regional_high/dataloader_vel.py
+++ regional_high/dataloader_vel.py
@@ -26,45 +26,22 @@ class train_Dataset(data_utils.Dataset):
 
     def __len__(self):
         return len(self.indices)
-    
-class test_Dataset(data_utils.Dataset):
-    def __init__(self, data_path):
-        super(test_Dataset, self).__init__()
-        self.years = range(2019, 2020)
-        self.dates = range(10, 350, 3)
-        self.data_path = data_path
-        self.indices = [(m, n) for m in self.years for n in self.dates]
-        
-    def __getitem__(self, index):
-        #print(index)
-        years, dates = self.indices[index]
-        train_data = nc.Dataset(f'{self.data_path}/geo_{years}.nc')
-        input = train_data.variables['mhws_variables'][dates-1:dates,:,0:-1:2,0:-1:2].filled(np.nan)
-        target = train_data.variables['mhws_variables'][dates:dates+1,:,0:-1:2,0:-1:2].filled(np.nan)
-        
-        input = torch.tensor(input)
-        target = torch.tensor(target)
-        input = torch.nan_to_num(input, nan=0)
-        target = torch.nan_to_num(target, nan=0)
-        return input, target
 
-    def __len__(self):
-        return len(self.indices)
-
+# for training \mathcal{M}_{\theta}, use this:
 # class test_Dataset(data_utils.Dataset):
 #     def __init__(self, data_path):
 #         super(test_Dataset, self).__init__()
-#         self.years = range(2020, 2021)
-#         self.dates = range(10, 240, 10)
+#         self.years = range(2019, 2020)
+#         self.dates = range(10, 350, 3)
 #         self.data_path = data_path
 #         self.indices = [(m, n) for m in self.years for n in self.dates]
         
 #     def __getitem__(self, index):
-#         print(index)
+#         #print(index)
 #         years, dates = self.indices[index]
 #         train_data = nc.Dataset(f'{self.data_path}/geo_{years}.nc')
-#         input = train_data.variables['mhws_variables'][dates-10:dates,:].filled(np.nan)
-#         target = train_data.variables['mhws_variables'][dates:dates+30,:].filled(np.nan)
+#         input = train_data.variables['mhws_variables'][dates-1:dates,:,0:-1:2,0:-1:2].filled(np.nan)
+#         target = train_data.variables['mhws_variables'][dates:dates+1,:,0:-1:2,0:-1:2].filled(np.nan)
         
 #         input = torch.tensor(input)
 #         target = torch.tensor(target)
@@ -74,3 +51,28 @@ class test_Dataset(data_utils.Dataset):
 
 #     def __len__(self):
 #         return len(self.indices)
+
+# for inference, use this:
+class test_Dataset(data_utils.Dataset):
+    def __init__(self, data_path):
+        super(test_Dataset, self).__init__()
+        self.years = range(2020, 2021)
+        self.dates = range(10, 240, 10)
+        self.data_path = data_path
+        self.indices = [(m, n) for m in self.years for n in self.dates]
+        
+    def __getitem__(self, index):
+        print(index)
+        years, dates = self.indices[index]
+        train_data = nc.Dataset(f'{self.data_path}/geo_{years}.nc')
+        input = train_data.variables['mhws_variables'][dates-10:dates,:].filled(np.nan)
+        target = train_data.variables['mhws_variables'][dates:dates+30,:].filled(np.nan)
+        
+        input = torch.tensor(input)
+        target = torch.tensor(target)
+        input = torch.nan_to_num(input, nan=0)
+        target = torch.nan_to_num(target, nan=0)
+        return input, target
+
+    def __len__(self):
+        return len(self.indices)
